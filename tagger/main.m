0ff86765ea3f1e93a19414cd46e34b2dbc06fe10
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-10-30 11:06:14 -0600

    Initial Commit

---
+++ tagger/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tagger
+//
+//  Created by Diego Magdaleno on 10/30/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
